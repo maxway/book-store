83171bfbb94da4e8f0f2b3751e485faf65b5e20b
Max.Mei <maxway@gmail.com>
2011-12-22 02:38:57 +0800

    working project

--- src/test/java/com/max/bookstore/BookClassDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.max.bookstore;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = BookClass.class)
-public class BookClassDataOnDemand {
-}
--- src/test/java/com/max/bookstore/model/StatusDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.max.bookstore.model;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Status.class)
-public class StatusDataOnDemand {
-}
